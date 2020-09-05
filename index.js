53860338ca6ae1c19817596cbc0567b42b60f137
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-05 23:04:48 +0530

    Changing the left image to a random dice

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber1 = Math.floor(Math.random() * 6 + 1); //Generates a random number from 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png-dice6.png;
+
+var randomImage = "images/" + randomDiceImage; // images/dice1.png-images/dice2.png
+
+var image1 = document.querySelectorAll("img")[0]; //Selecting the first image element
+
+image1.setAttribute("src", randomImage); //Setting the attribute to a random dice
